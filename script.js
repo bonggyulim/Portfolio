d667893fb00b418d4eb10fa960fe2a8084b67658
bonggyulim <qhdrbdhkdwks@naver.com>
2026-03-22 17:14:31 +0900

    feat: build portfolio landing page

--- script.js
+++ script.js
@@ -3,12 +3,6 @@ const navLinks = document.querySelector(".nav-links");
 
 if (menuToggle && navLinks) {
   menuToggle.addEventListener("click", () => {
-    navLinks.classList.toggle("is-open");
-  });
-
-  navLinks.querySelectorAll("a").forEach((link) => {
-    link.addEventListener("click", () => {
-      navLinks.classList.remove("is-open");
-    });
+    navLinks.classList.toggle("open");
   });
 }
